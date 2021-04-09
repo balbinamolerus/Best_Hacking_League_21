b710c053a361950a05a3907ca889e9b93de1d0e3
balbinamolerus <bmolerus@student.agh.edu.pl>
2021-04-09 18:29:20 +0200

    initial

--- rpi72.py
+++ rpi72.py
@@ -1,18 +1,18 @@
 import RPi.GPIO as GPIO
-# import paho.mqtt.client as mqtt
-# import time
+import paho.mqtt.client as mqtt
+import time
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 GPIO.setup(2, GPIO.IN)
 
-# broker_address = "192.168.1.200"
-# client = mqtt.Client()
-# client.username_pw_set("Raspberry_Pi", "Rpi_Raspberry_Python")
-# client.on_message = on_message
-# client.connect(broker_address, 1883)
-# client.loop_start()
+broker_address = "192.168.1.200"
+client = mqtt.Client()
+client.username_pw_set("Raspberry_Pi", "Rpi_Raspberry_Python")
+client.on_message = on_message
+client.connect(broker_address, 1883)
+client.loop_start()
 
 before = 0
 now = 0
@@ -22,6 +22,7 @@ try:
         now = GPIO.input(2)
         if now == 1 and before == 0:
             print('alarm')
+            client.publish("FireAlarm/Alarm", "1", qos=1, retain=True)
         before = GPIO.input(2)
 except KeyboardInterrupt:
     print('Koniec')
