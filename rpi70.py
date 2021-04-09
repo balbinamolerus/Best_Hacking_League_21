ecf19f3fa2836d0d0cdd65129cb237cbd33d9ebd
pudliss <jakub.pudlo@outlook.com>
2021-04-09 23:27:12 +0200

    buttons

--- rpi70.py
+++ rpi70.py
@@ -9,7 +9,18 @@ client.connect(broker_address, 1883)
 client.loop_start()
 
 button = Button(21)
-
+time=0
 while True:
-  if button.is_pressed == False:
-      print("Alarm")
+    x = 0
+    time = 0
+    while time<1000:
+        if button.is_pressed == False:
+            x=x+1
+        sleep(0.1)
+        if x > 800:
+            print('alarm')
+            #client.publish("BHL/FattyAlarm/Alarm", "1", qos=1, retain=True)
+            x = 0
+            sleep(20)
+        time = time + 1
+
