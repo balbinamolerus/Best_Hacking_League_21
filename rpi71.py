77d8429bc4a9201635b44f18df6d6cdf15f4bdd0
pudliss <jakub.pudlo@outlook.com>
2021-04-09 21:13:28 +0200

    mqtt

--- rpi71.py
+++ rpi71.py
@@ -1,6 +1,12 @@
 from time import sleep
 from gpiozero import MotionSensor
+import paho.mqtt.client as mqtt
 
+broker_address = "192.168.1.200"
+client = mqtt.Client()
+client.username_pw_set("Raspberry_Pi", "Rpi_Raspberry_Python")
+client.connect(broker_address, 1883)
+client.loop_start()
 
 pir = MotionSensor(4)
 
@@ -12,7 +18,8 @@ while True:
       x=x+1
     sleep(0.1)
     if x>20:
-      print("ALARM")
+      print('alarm')
+      client.publish("BHL/MoveAlarm/Alarm", "1", qos=1, retain=True)
       x=0
       print(t)
       sleep(10)
