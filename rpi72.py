c1a1de2924f0e27053d75383ee88a61ef742efd8
balbinamolerus <bmolerus@student.agh.edu.pl>
2021-04-09 18:16:16 +0200

    initial

--- rpi72.py
+++ rpi72.py
@@ -1,12 +1,28 @@
 import RPi.GPIO as GPIO
+import paho.mqtt.client as mqtt
+import time
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 GPIO.setup(2, GPIO.IN)
 
+broker_address = "192.168.1.200"
+client = mqtt.Client()
+client.username_pw_set("Raspberry_Pi", "Rpi_Raspberry_Python")
+client.on_message = on_message
+client.connect(broker_address, 1883)
+client.loop_start()
+
+before = 0
+now = 0
 try:
     while True:
-        print(GPIO.input(2))
+        now = GPIO.input(2)
+        if now == 1 and before == 0:
+            print(before, now, '\n alarm')
+        else:
+            print(before, now)
+        before = GPIO.input(2)
 except KeyboardInterrupt:
     print('Koniec')
