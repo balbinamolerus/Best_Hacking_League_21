5a275c1f422f1ab0357eefe05897e06bd0080010
balbinamolerus <bmolerus@student.agh.edu.pl>
2021-04-09 22:10:17 +0200

    initial

--- rpi73.py
+++ rpi73.py
@@ -59,7 +59,7 @@ dioda.start(wypelnienie)  # Uruchomienie sygnału PWM
 
 try:
     while True:
-        wypelnienie = input("wypelnienie")
+        wypelnienie = int(input("wypelnienie"))
         if wypelnienie > 100:
             wypelnienie = 0
         dioda.ChangeDutyCycle(wypelnienie)  # Ustaw nową wartość wypełnienia
