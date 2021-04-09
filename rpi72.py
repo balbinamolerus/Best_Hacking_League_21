6db45624997599b3d91d20ec4778dbd0625248bd
maciekbaczmanski <cakeisalie16@gmail.com>
2021-04-09 19:12:39 +0200

    html check

--- rpi72.py
+++ rpi72.py
@@ -30,7 +30,7 @@ try:
         html = urllib.request.urlopen('http://192.168.1.82/')
 
         htmltext = html.read()
-        print(htmltext)
+        print(type(htmltext))
         # except:
         #     print('error opening link')
 
