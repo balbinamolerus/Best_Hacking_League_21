1dcd886da800123a4c33010990d657d7da30f382
pudliss <jakub.pudlo@outlook.com>
2021-04-09 20:51:07 +0200

    9

--- rpi71.py
+++ rpi71.py
@@ -7,11 +7,11 @@ pir = MotionSensor(4)
 while True:
   x=0
   t=0
-  while t<1000:
+  while t<50:
     if pir.wait_for_motion()==True:
       x=x+1
     sleep(0.1)
-    if x>10:
+    if x>30:
       print("ALARM")
       x=0
       print(t)
