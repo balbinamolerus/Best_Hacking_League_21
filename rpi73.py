c8b0406cd31e175b5f128b7264aa0b31e8fe0d01
balbinamolerus <bmolerus@student.agh.edu.pl>
2021-04-09 19:54:06 +0200

    initial

--- rpi73.py
+++ rpi73.py
@@ -25,11 +25,11 @@ sensor_args = { '11': Adafruit_DHT.DHT11,
 #     print('Usage: sudo ./Adafruit_DHT.py [11|22|2302] <GPIO pin number>')
 #     print('Example: sudo ./Adafruit_DHT.py 2302 4 - Read from an AM2302 connected to GPIO pin #4')
 #     sys.exit(1)
-
+sensor = Adafruit_DHT.DHT11
 
 try:
     while True:
-        humidity, temperature = Adafruit_DHT.read_retry(sensor_args['11'], '2')
+        humidity, temperature = Adafruit_DHT.read_retry(sensor, '2')
         if humidity is not None and temperature is not None:
             print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
         else:
