7529d4cb9ef4381bfeb6ce3d2e34c61de131e982
balbinamolerus <bmolerus@student.agh.edu.pl>
2021-04-10 00:14:53 +0200

    initial

--- rpi73.py
+++ rpi73.py
@@ -55,14 +55,22 @@ GPIO.setwarnings(False)
 GPIO.setup(21, GPIO.OUT)
 
 Alarm = False
+Brightness = 0
+
+
 def on_message(client, userdata, message):
-    global Alarm
-    if message.topic == "BHL/MoveAlarm/Alarm" or message.topic == "BHL/WaterAlarm/Alarm" or message.topic == "BHL/FireAlarm/Alarm" :
+    global Alarm, Brightness
+    if message.topic == "BHL/MoveAlarm/Alarm" or message.topic == "BHL/WaterAlarm/Alarm" or message.topic == "BHL/FireAlarm/Alarm":
         Alarm = True
 
-    if message.topic == "BHL/StopAlarm" and str(message.payload.decode("utf-8")) == "1":
+    if message.topic == "BHL/StopAlarm":
         Alarm = False
 
+    if message.topic == "BHL/LED/setOn" and str(message.payload.decode("utf-8")) == "0":
+        Brightness = 0
+
+    if message.topic == "BHL/LED/setBrightness":
+        Brightness = int(message.payload.decode("utf-8"))
 
 
 broker_address = "192.168.1.200"
@@ -73,7 +81,8 @@ client.connect(broker_address, 1883)
 client.loop_start()
 
 client.subscribe(
-    [("BHL/MoveAlarm/Alarm", 1), ("BHL/FireAlarm/Alarm", 1), ("BHL/WaterAlarm/Alarm", 1),("BHL/StopAlarm", 1)])
+    [("BHL/MoveAlarm/Alarm", 1), ("BHL/FireAlarm/Alarm", 1), ("BHL/WaterAlarm/Alarm", 1), ("BHL/StopAlarm", 1),
+     ("BHL/LED/setOn", 1), ("BHL/LED/setBrightness", 1)])
 
 dioda = GPIO.PWM(21, 980)  # Nowa instancja PWM
 wypelnienienew = 0  # Wypełnienie sygnału PWM
@@ -82,12 +91,13 @@ dioda.start(wypelnienienew)  # Uruchomienie sygnału PWM
 
 updown = True
 wypelnienie = 0
+oldBrightness = 0
 try:
     while True:
         if Alarm:
             if updown == True:
                 dioda.ChangeDutyCycle(wypelnienie)
-                wypelnienie = wypelnienie+2
+                wypelnienie = wypelnienie + 2
                 if wypelnienie == 100:
                     updown = False
             else:
@@ -96,6 +106,18 @@ try:
                 if wypelnienie == 2:
                     updown = True
             time.sleep(0.01)
+        else:
+            if Brightness != oldBrightness:
+                dioda.ChangeDutyCycle(Brightness)
+                client.publish("BHL/LED/getBrightness", str(Brightness), qos=1, retain=True)
+                if Brightness:
+                    client.publish("BHL/LED/getOn", "1", qos=1, retain=True)
+                else:
+                    client.publish("BHL/LED/getOn", "0", qos=1, retain=True)
+                oldBrightness = Brightness
+
+
+
 except KeyboardInterrupt:
     print('Koniec')
 
