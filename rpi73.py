c87c63ca84a32009f961133ba6d069865477b8c3
balbinamolerus <bmolerus@student.agh.edu.pl>
2021-04-09 22:07:50 +0200

    initial

--- rpi73.py
+++ rpi73.py
@@ -53,13 +53,13 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(12, GPIO.OUT)
 
-dioda = GPIO.PWM(12, 50)  # Nowa instancja PWM
+dioda = GPIO.PWM(12, 5000)  # Nowa instancja PWM
 wypelnienie = 0  # Wypełnienie sygnału PWM
 dioda.start(wypelnienie)  # Uruchomienie sygnału PWM
 
 try:
     while True:
-        wypelnienie += 5
+        wypelnienie += 1
         if wypelnienie > 100:
             wypelnienie = 0
         dioda.ChangeDutyCycle(wypelnienie)  # Ustaw nową wartość wypełnienia
