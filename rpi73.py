fe0123d2e711d5950be146faf8898a62f780be62
balbinamolerus <bmolerus@student.agh.edu.pl>
2021-04-09 19:50:02 +0200

    initial

--- rpi73.py
+++ rpi73.py
@@ -18,19 +18,18 @@ client.loop_start()
 sensor_args = { '11': Adafruit_DHT.DHT11,
                 '22': Adafruit_DHT.DHT22,
                 '2302': Adafruit_DHT.AM2302 }
-if len(sys.argv) == 3 and sys.argv[1] in sensor_args:
-    sensor = sensor_args[sys.argv[1]]
-    pin = sys.argv[2]
-else:
-    print('Usage: sudo ./Adafruit_DHT.py [11|22|2302] <GPIO pin number>')
-    print('Example: sudo ./Adafruit_DHT.py 2302 4 - Read from an AM2302 connected to GPIO pin #4')
-    sys.exit(1)
+# if len(sys.argv) == 3 and sys.argv[1] in sensor_args:
+#     sensor = sensor_args[sys.argv[1]]
+#     pin = sys.argv[2]
+# else:
+#     print('Usage: sudo ./Adafruit_DHT.py [11|22|2302] <GPIO pin number>')
+#     print('Example: sudo ./Adafruit_DHT.py 2302 4 - Read from an AM2302 connected to GPIO pin #4')
+#     sys.exit(1)
 
 
 try:
     while True:
-        humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
-
+        humidity, temperature = Adafruit_DHT.read_retry(sensor_args['11'], '2')
         if humidity is not None and temperature is not None:
             print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
         else:
