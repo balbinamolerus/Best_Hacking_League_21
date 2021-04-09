100c9a1e6ff5422b7100b4110cac1c7cd6b49dbb
balbinamolerus <bmolerus@student.agh.edu.pl>
2021-04-09 19:33:47 +0200

    initial

--- rpi73.py
+++ rpi73.py
@@ -1,12 +1,13 @@
 import RPi.GPIO as GPIO
 import paho.mqtt.client as mqtt
+import sys
+import Adafruit_DHT
+
 import time
 import urllib
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-
-GPIO.setup(2, GPIO.OUT)
 
 broker_address = "192.168.1.200"
 client = mqtt.Client()
@@ -14,3 +15,26 @@ client.username_pw_set("Raspberry_Pi", "Rpi_Raspberry_Python")
 client.connect(broker_address, 1883)
 client.loop_start()
 
+sensor_args = { '11': Adafruit_DHT.DHT11,
+                '22': Adafruit_DHT.DHT22,
+                '2302': Adafruit_DHT.AM2302 }
+if len(sys.argv) == 3 and sys.argv[1] in sensor_args:
+    sensor = sensor_args[sys.argv[1]]
+    pin = sys.argv[2]
+else:
+    print('Usage: sudo ./Adafruit_DHT.py [11|22|2302] <GPIO pin number>')
+    print('Example: sudo ./Adafruit_DHT.py 2302 4 - Read from an AM2302 connected to GPIO pin #4')
+    sys.exit(1)
+
+
+try:
+    while True:
+        humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+
+        if humidity is not None and temperature is not None:
+            print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
+        else:
+            print('Failed to get reading. Try again!')
+            sys.exit(1)
+except KeyboardInterrupt:
+    print('Koniec')
