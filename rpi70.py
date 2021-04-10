06c003cdeb694986c9dbf1a7bbb3316f36565a99
maciekbaczmanski <cakeisalie16@gmail.com>
2021-04-10 09:41:54 +0200

    html check

--- rpi70.py
+++ rpi70.py
@@ -1,6 +1,7 @@
 from time import sleep
 from gpiozero import Button
 import paho.mqtt.client as mqtt
+import time
 
 broker_address = "192.168.1.200"
 client = mqtt.Client()
@@ -10,26 +11,36 @@ client.loop_start()
 
 button = Button(21)
 last_button = True
-open_count = False
-while True:
-    x = 0
-    time = 0
-    while time<100:
-        if button.is_pressed == False:
-            x=x+1
-        sleep(0.05)
-        if x > 50:
-            client.publish("BHL/FridgeAlarm/Alarm", "1", qos=1, retain=True)
-            print('alarm')
-            x = 0
-            sleep(20)
-        time = time + 1
 
+open_count = False
+
+while True:
+    # x = 0
+    # time = 0
+    # while time < 100:
+    #     if button.is_pressed == False:
+    #         x = x + 1
+    #     sleep(0.05)
+    #     if x > 50:
+    #         client.publish("BHL/FridgeAlarm/Alarm", "1", qos=1, retain=True)
+    #         print('alarm')
+    #         x = 0
+    #         sleep(20)
+    #     time = time + 1
 
     while True:
-        if not button.is_pressed and last_button:
+        new_button = button.is_pressed
+        if not new_button and last_button:
             client.publish("BHL/Fridge", "1", qos=1, retain=True)
+            starttime = time.time()
+            open_count = True
 
-
-        if button.is_pressed and not last_button:
+        if new_button and not last_button:
             client.publish("BHL/Fridge", "0", qos=1, retain=True)
+            client.publish("BHL/FridgeAlarm/Alarm", "0", qos=1, retain=True)
+            open_count = False
+
+        if time.time() - starttime > 10 and open_count:
+            client.publish("BHL/FridgeAlarm/Alarm", "1", qos=1, retain=True)
+
+        last_button = new_button
