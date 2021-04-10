cef476c8c0b4db559e920b6f714f3afd06f500a0
pudliss <jakub.pudlo@outlook.com>
2021-04-10 10:23:11 +0200

    MoveAlarmxc

--- rpi75.py
+++ rpi75.py
@@ -2,10 +2,16 @@ from time import sleep
 import RPi.GPIO as GPIO
 import paho.mqtt.client as mqtt
 
+def on_message(client, userdata, message):
+    global Alarm, Brightness
+    if message.topic == "BHL/MoveAlarm/Alarm":
+        Alarm = True
+
 broker_address = "192.168.1.200"
 client = mqtt.Client("Control_Interface")
 client.username_pw_set("Raspberry_Pi", "Rpi_Raspberry_Python")
 client.connect(broker_address, 1883)
+client.on_message = on_message
 client.loop_start()
 client.subscribe([("BHL/MoveAlarm/Alarm", 1)])
 
@@ -16,8 +22,9 @@ GPIO.setup(2, GPIO.OUT)
 while True:
     now = GPIO.input(17)
     sleep(0.2)
-    if now == 0 or client.on_message.topic == "BHL/MoveAlarm/Alarm"=="BHL/MoveAlarm/Alarm":
+    if now == 0 or Alarm==True:
         print('alarm')
+        Alarm == False
         GPIO.output(2, GPIO.LOW)
     else:
         GPIO.output(2, GPIO.HIGH)
