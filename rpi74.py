72bd3c76f6508ed535a84d6b63b3cc7cce28a296
maciekbaczmanski <cakeisalie16@gmail.com>
2021-04-10 11:55:10 +0200

    html check

--- rpi74.py
+++ rpi74.py
@@ -1,8 +1,7 @@
 import RPi.GPIO as GPIO
 import paho.mqtt.client as mqtt
 import time
-import adafruit_dht
-import board
+
 # import pygame
 
 
@@ -13,7 +12,7 @@ GPIO.setup(21, GPIO.OUT)
 Alarm = False
 Brightness = 0
 
-dht_device = adafruit_dht.DHT11(board.D26)
+
 # pygame.mixer.init()
 # pygame.mixer.music.load('alarm.mp3')
 # pygame.mixer.music.set_volume(1.0)
@@ -23,13 +22,10 @@ def on_message(client, userdata, message):
     global Alarm, Brightness
     if message.topic == "BHL/MoveAlarm/Alarm" or message.topic == "BHL/WaterAlarm/Alarm" or message.topic == "BHL/FireAlarm/Alarm":
         Alarm = True
-        # if not pygame.mixer.music.get_busy():
-        #     pygame.mixer.music.play(loops=-1)
+
 
     if message.topic == "BHL/StopAlarm":
         Alarm = False
-        # if pygame.mixer.music.get_busy():
-        #     pygame.mixer.music.stop()
 
     if message.topic == "BHL/LED/setOn":
         if str(message.payload.decode("utf-8")) == "0":
@@ -93,19 +89,7 @@ try:
                 else:
                     client.publish("BHL/LED/getOn", "0", qos=1, retain=True)
                 oldBrightness = Brightness
-        counter += 1
-        if counter == 100:
-            temperature = dht_device.temperature
-            humidity = dht_device.humidity
-            if temperature != last_temperature:
-                client.publish("BHL/temperature", str(temperature), qos=1, retain=True)
 
-            if humidity != last_humidity:
-                client.publish("BHL/humidity", str(humidity), qos=1, retain=True)
-
-            last_humidity = humidity
-            last_temperature = temperature
-            counter = 0
 
         time.sleep(0.01)
 
