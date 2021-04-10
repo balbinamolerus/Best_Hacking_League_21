1e48ad73f666e5f04fb9e377a8742d4e89f3fbb6
pudliss <jakub.pudlo@outlook.com>
2021-04-10 02:15:39 +0200

    alarm 75

---
+++ rpi75.py
@@ -0,0 +1,14 @@
+from time import sleep
+from gpiozero import Button
+import paho.mqtt.client as mqtt
+
+broker_address = "192.168.1.200"
+client = mqtt.Client()
+client.username_pw_set("Raspberry_Pi", "Rpi_Raspberry_Python")
+client.connect(broker_address, 1883)
+client.loop_start()
+
+button = Button(4)
+
+if button.is_pressed==False:
+    print("Alarm")
