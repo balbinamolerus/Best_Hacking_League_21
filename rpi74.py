c0df06ef0a6b63b80f9044496d13a603139dd219
balbinamolerus <bmolerus@student.agh.edu.pl>
2021-04-10 03:19:43 +0200

    initial

--- rpi74.py
+++ rpi74.py
@@ -1,56 +1,10 @@
-# import RPi.GPIO as GPIO
-# import paho.mqtt.client as mqtt
-# import sys
-# import adafruit_dht
-# import time
-# import board
-# import urllib
-#
-# GPIO.setmode(GPIO.BCM)
-# GPIO.setwarnings(False)
-#
-# broker_address = "192.168.1.200"
-# client = mqtt.Client()
-# client.username_pw_set("Raspberry_Pi", "Rpi_Raspberry_Python")
-# client.connect(broker_address, 1883)
-# client.loop_start()
-# #
-# dht_device = adafruit_dht.DHT11(board.D4)
-#
-# # sensor_args = { '11': Adafruit_DHT.DHT11,
-# #                 '22': Adafruit_DHT.DHT22,
-# #                 '2302': Adafruit_DHT.AM2302 }
-# # if len(sys.argv) == 3 and sys.argv[1] in sensor_args:
-# #     sensor = sensor_args[sys.argv[1]]
-# #     pin = sys.argv[2]
-# # else:
-# #     print('Usage: sudo ./Adafruit_DHT.py [11|22|2302] <GPIO pin number>')
-# #     print('Example: sudo ./Adafruit_DHT.py 2302 4 - Read from an AM2302 connected to GPIO pin #4')
-# #     sys.exit(1)
-#
-#
-# try:
-#     while True:
-#         temperature = dht_device.temperature
-#         humidity = dht_device.humidity
-#         if humidity is not None and temperature is not None:
-#             print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
-#         else:
-#             print('Failed to get reading. Try again!')
-#             sys.exit(1)
-#         if humidity>70:
-#             client.publish("BHL/HumidityAlarm/Alarm", "1", qos=1, retain=True)
-#         time.sleep(1)
-#
-# except KeyboardInterrupt:
-#     print('Koniec')
-
-
 import RPi.GPIO as GPIO
 import paho.mqtt.client as mqtt
 import time
 import adafruit_dht
 import board
+import pygame
+
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
@@ -60,17 +14,22 @@ Alarm = False
 Brightness = 0
 
 dht_device = adafruit_dht.DHT11(board.D26)
-
-
+pygame.mixer.init()
+pygame.mixer.music.load('alarm.mp3')
+pygame.mixer.music.set_volume(0.1)
 
 
 def on_message(client, userdata, message):
     global Alarm, Brightness
     if message.topic == "BHL/MoveAlarm/Alarm" or message.topic == "BHL/WaterAlarm/Alarm" or message.topic == "BHL/FireAlarm/Alarm":
         Alarm = True
+        if not pygame.mixer.music.get_busy():
+            pygame.mixer.music.play(loops=-1)
 
     if message.topic == "BHL/StopAlarm":
         Alarm = False
+        if pygame.mixer.music.get_busy():
+            pygame.mixer.music.stop()
 
     if message.topic == "BHL/LED/setOn":
         if str(message.payload.decode("utf-8")) == "0":
