48e6925b99ab85ace9faff603738372a01bbc545
maciekbaczmanski <cakeisalie16@gmail.com>
2021-04-10 03:57:43 +0200

    html check

--- rpi72.py
+++ rpi72.py
@@ -9,7 +9,7 @@ GPIO.setwarnings(False)
 
 GPIO.setup(2, GPIO.IN)
 
-pygame.mixer.init(devicename=None)
+pygame.mixer.init()
 pygame.mixer.music.load('alarm.mp3')
 pygame.mixer.music.set_volume(0.5)
 
